b015784b153e4b090d3ec9ebfc99a9572862f091
soni-world <sonikumari116@gmail.com>
2025-06-13 19:48:08 +0400

    test

---
+++ src/components/Counter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, incrementByAmount } from '../features/counterSlice';
+function Counter() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <p>Current Count: {count}</p>
+      <button onClick={() => dispatch(increment())}>Increase</button>
+      <button onClick={() => dispatch(decrement())}>Decrease</button>
+      <button onClick={() => dispatch(incrementByAmount(5))}>Increase by 5</button>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import "bootstrap/dist/css/bootstrap.css";
-import "font-awesome/css/font-awesome.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { Provider } from 'react-redux';
+import store from './store';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
